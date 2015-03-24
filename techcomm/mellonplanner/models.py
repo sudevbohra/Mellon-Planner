fc089f15d69aa936bb8bed76cd924d8b310f3fbd
Sudev <sudevbohra@cmu.edu>
2015-03-24 03:12:35 -0400

    Created models for backend

--- techcomm/mellonplanner/models.py
+++ techcomm/mellonplanner/models.py
@@ -11,3 +11,25 @@ class Profile(models.Model):
 
     def __unicode__(self):
         return self.first_name + " " + self.last_name
+
+
+class Day(models.Model):
+	name = models.CharField(max_length=10)
+
+class Lecture(models.Model):
+	days = models.ManyToManyField(Day, related_name = "lectures")
+	time_start = models.TimeField(blank = True)
+	time_end = models.TimeField(blank = True)
+	recitations = models.ManyToManyField(Recitation, related_name = "lectures")
+
+
+class Recitation(models.Model):
+	days = models.ManyToManyField(Day, related_name = "recitations")
+	time_start = models.TimeField(blank = True)
+	time_end = models.TimeField(blank = True)
+
+class Course(models.Model):
+	course_id = models.IntegerField(max_length=7)
+	name = models.CharField(max_length=100)
+	lectures = models.ManyToManyField(Lecture, related_name = "course")
+	units = models.IntegerField(max_length=2)
