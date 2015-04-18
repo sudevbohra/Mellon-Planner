394a56426e7eebf0f28e65615427b522de53c537
Sudev <sudevbohra@cmu.edu>
2015-04-18 08:43:58 -0400

    pages can be changed now

--- techcomm/mellonplanner/urls.py
+++ techcomm/mellonplanner/urls.py
@@ -2,12 +2,8 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    url(r'^$', 'mellonplanner.views.home'),
-    url(r'^login', 'django.contrib.auth.views.login', {'template_name':'Login_Page.html'}),
-    url(r'^logout$', 'django.contrib.auth.views.logout_then_login'),
-    url(r'^register/$', 'mellonplanner.views.register'),
-    url(r'^myschedule/', 'mellonplanner.views.home'),
-    url(r'^login/', 'mellonplanner.views.home'),
+    url(r'^$', 'mellonplanner.views.home', name="home"),
     url(r'^getschedules/', 'mellonplanner.views.getschedules'),
+    url(r'^getschedule/(?P<index>\d+)$', 'mellonplanner.views.getschedule', name="getschedule"),
     #url(r'', 'mellonplanner.views.mellon'),
 )
--- techcomm/mellonplanner/views.py
+++ techcomm/mellonplanner/views.py
@@ -13,9 +13,29 @@ from mellonplanner.backend.sched import getAllSchedules
 
 from django.core.context_processors import csrf
 
-@login_required
+def getschedule(request, index):
+    index = int(index)
+    context = {}
+    if 'schedules' not in request.session:
+        context['errors'] = ["Reinput classes or check cookies"]
+        return render(request, 'index.html', context)
+    if index > len(request.session['schedules']):
+        context['errors'] = [str(type(request.session['schedules']))]
+        return render(request, 'index.html', context)
+    allListFormatted,unitsList = request.session['schedules'],request.session['units']
+    context['schedule'] = allListFormatted[index]
+
+    #context['scheduleCount'] = len(allListFormatted)
+    
+    context['unitsList'] = enumerate(unitsList)
+    context['currentIndex'] = index
+    # and finally put pictures in the context dictionary
+    return render(request, 'index.html', context)
+
+
 def getschedules(request):
-    profile = Profile.objects.get(user=request.user)
+    
+
     context = {}
     context.update(csrf(request))
     errors = []
@@ -27,39 +47,59 @@ def getschedules(request):
     if not 'maxunits' in request.POST or not request.POST['maxunits']:
         errors.append('Maximum units is required')
     if errors:
-        return render(request, 'Hello.html', context)
+        return render(request, 'index.html', context)
     if(int(request.POST['minunits']) < 0 or
        (int(request.POST['maxunits']) < int(request.POST['minunits']))):
         errors.append('Invalid units')
     list_of_classes = request.POST['loc'].replace(' ', '').split(",")
     # Should check for validity of classes here, and add errors if any
-    if errors:
-        return render(request, 'Hello.html', context)
     # Then call the backend functions
+    try:
+        schedules = getAllSchedules(list_of_classes)
 
-    schedules = getAllSchedules(list_of_classes)
+    except Exception:
+        context['errors'] += "Something went wrong with course names. May not exist."
+        return render(request, 'index.html', context)
+
+
+
     #print(schedules)
     #print(list_of_classes)
-    listFormatted = []
-    schedule1 = schedules[0]
-    units = schedule1[0]
-    classList = schedule1[1]
+    allListFormatted = []
+    unitsList = []
+    for schedule in schedules:
+        listFormatted = []
 
-    for cls in classList:
-        listFormatted.extend(convertTimeList(cls[0],cls[1]))
+        schedule1 = schedule
+        units = schedule1[0]
+        print units
+        classList = schedule1[1]
+
+        for cls in classList:
+
+            listFormatted.extend(convertTimeList(cls[0],cls[1]))
+        allListFormatted += [listFormatted]
+        unitsList += [units]
+
 
     #print listFormatted
     #('15122 Lec 2 N', 20, [(0, 12.5, 13.5), (1, 10.5, 12.0), (3, 10.5, 12.0)]), ('21127 Lec 2 H', 20, [(0, 14.5, 15.5), (1, 13.5, 14.5), (2, 14.5, 15.5), (3, 13.5, 14.5), (4, 14.5, 15.5)])
     context['schedule'] = listFormatted
 
+    #context['scheduleCount'] = len(allListFormatted)
+    request.session['schedules'],request.session['units'] = allListFormatted,unitsList
+    context['unitsList'] = enumerate(unitsList)
+    context['currentIndex'] = 0
     # and finally put pictures in the context dictionary
-    return render(request, 'Hello.html', context)
+    return render(request, 'index.html', context)
 
 numberToDate = {-1: '2015-02-08T', 0: '2015-02-09T', 1: '2015-02-10T', 2: '2015-02-11T', 3:'2015-02-12T', 4:'2015-02-13T', 5:'2015-02-14T', 6:'2015-02-15T'}
 
 
 
 def convertTimeList(klass, l):
+    #Test cookie
+    
     formattedL = []
     for day,start,end in l:
         #HH:MM:SS
@@ -73,76 +113,78 @@ def convertTimeList(klass, l):
     return formattedL
 
 
-@login_required
+
 def home(request):
-    try:
-        profile = Profile.objects.get(user=request.user)
-    except ObjectDoesNotExist:
-        return render(request, 'Login_Page.html', {})
+    
+    request.session.set_test_cookie()
+    # try:
+    #     profile = Profile.objects.get(user=request.user)
+    # except ObjectDoesNotExist:
+    #     return render(request, 'Login_Page.html', {})
     context = {}
-    context.update(csrf(request))
-    return render(request, 'Hello.html', context)
+    # context.update(csrf(request))
+    return render(request, 'index.html', context)
 
-def register(request):
-    context = {}
-    context.update(csrf(request))
+# def register(request):
+#     context = {}
+#     context.update(csrf(request))
 
-    # Just display the registration form if this is a GET request
-    if request.method == 'GET':
-        return render(request, 'Login_Page.html', context)
+#     # Just display the registration form if this is a GET request
+#     if request.method == 'GET':
+#         return render(request, 'Login_Page.html', context)
 
-    errors = []
-    context['errors'] = errors
+#     errors = []
+#     context['errors'] = errors
 
-    # Checks the validity of the form data
-    if not 'username1' in request.POST or not request.POST['username1']:
-	errors.append('Username is required.')
-    else:
-        # Save the username in the request context to re-fill the username
-        # field in case the form has errrors
-	context['username'] = request.POST['username1']
-    if not 'username2' in request.POST or not request.POST['username2']:
-	errors.append('Confirm username is required.')
+#     # Checks the validity of the form data
+#     if not 'username1' in request.POST or not request.POST['username1']:
+# 	errors.append('Username is required.')
+#     else:
+#         # Save the username in the request context to re-fill the username
+#         # field in case the form has errrors
+# 	context['username'] = request.POST['username1']
+#     if not 'username2' in request.POST or not request.POST['username2']:
+# 	errors.append('Confirm username is required.')
 
-    if 'username1' in request.POST and 'username2' in request.POST \
-       and request.POST['username1'] and request.POST['username2'] \
-       and request.POST['username1'] != request.POST['username2']:
-	errors.append('Usernames did not match.')
+#     if 'username1' in request.POST and 'username2' in request.POST \
+#        and request.POST['username1'] and request.POST['username2'] \
+#        and request.POST['username1'] != request.POST['username2']:
+# 	errors.append('Usernames did not match.')
 
-    if not 'password1' in request.POST or not request.POST['password1']:
-	errors.append('Password is required.')
-    if not 'password2' in request.POST or not request.POST['password2']:
-	errors.append('Confirm password is required.')
+#     if not 'password1' in request.POST or not request.POST['password1']:
+# 	errors.append('Password is required.')
+#     if not 'password2' in request.POST or not request.POST['password2']:
+# 	errors.append('Confirm password is required.')
 
-    if 'password1' in request.POST and 'password2' in request.POST \
-       and request.POST['password1'] and request.POST['password2'] \
-       and request.POST['password1'] != request.POST['password2']:
-	errors.append('Passwords did not match.')
+#     if 'password1' in request.POST and 'password2' in request.POST \
+#        and request.POST['password1'] and request.POST['password2'] \
+#        and request.POST['password1'] != request.POST['password2']:
+# 	errors.append('Passwords did not match.')
 
-    if len(User.objects.filter(username = request.POST['username1'])) > 0:
-	errors.append('There is already an account linked to that username')
+#     if len(User.objects.filter(username = request.POST['username1'])) > 0:
+# 	errors.append('There is already an account linked to that username')
 
-    if errors:
-        return render(request, 'Login_Page.html', context)
+#     if errors:
+#         return render(request, 'Login_Page.html', context)
 
-    # Creates the new user from the valid form data
-    new_user = User.objects.create_user(username=request.POST['username1'], \
-                                        password=request.POST['password1'])
-    new_user.save()
+#     # Creates the new user from the valid form data
+#     new_user = User.objects.create_user(username=request.POST['username1'], \
+#                                         password=request.POST['password1'])
+#     new_user.save()
 
-    username = new_user.get_username()
-    fname = ''
-    lname = ''
-    if 'fname' in request.POST and request.POST['fname']:
-        fname = request.POST['fname']
-    if 'lname' in request.POST and request.POST['lname']:
-        lname = request.POST['lname']
-    new_user_profile = Profile(user=new_user, first_name=fname, \
-                               last_name=lname, username=username)
-    new_user_profile.save()
+#     username = new_user.get_username()
+#     fname = ''
+#     lname = ''
+#     if 'fname' in request.POST and request.POST['fname']:
+#         fname = request.POST['fname']
+#     if 'lname' in request.POST and request.POST['lname']:
+#         lname = request.POST['lname']
+#     new_user_profile = Profile(user=new_user, first_name=fname, \
+#                                last_name=lname, username=username)
+#     new_user_profile.save()
 
-    # Logs in the new user and redirects to his/her profile
-    new_user = authenticate(username=request.POST['username1'], \
-                            password=request.POST['password1'])
-    login(request, new_user)
-    return redirect('/myschedule/')
+#     # Logs in the new user and redirects to his/her profile
+#     new_user = authenticate(username=request.POST['username1'], \
+#                             password=request.POST['password1'])
+#     login(request, new_user)
+#     return redirect('/myschedule/')
